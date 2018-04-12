14548999f0362ca429eb5013733761ec7c151816
dluchau <dani032e@edu.eal.dk>
2018-04-12 23:02:06 +0200

    var

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,19 @@
-$(document).ready(function() {
-    var counter = 0;
-    if(localStorage.getItem("count") == null)
-        $("#count").text(0);
-    else {
-        counter = localStorage.getItem("count");
-        $("#count").text(counter);
+var popup = document.getElementById('newpostpopup');
+
+var btn = document.getElementById("newpostbutton");
+
+var button = document.getElementsByClassName("closebutton")[0];
+
+btn.onclick = function() {
+    popup.style.display = "block";
+}
+
+button.onclick = function() {
+    popup.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == popup) {
+        popup.style.display = "none";
     }
-    
-    $("#plus").click(function(){
-      counter++;
-      $("#count").text(counter);
-      localStorage.setItem("count", counter);
-    });
-    
-    $("#minus").click(function(){
-      counter--;    
-      $("#count").text(counter);
-      localStorage.setItem("count", counter);
-    });
-});
-
-
-
-
+}

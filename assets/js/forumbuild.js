0134c89180f80f479652e3d7d19d0162a211adec
Laurakathrine <arual1044@hotmail.com>
2018-04-16 11:00:34 +0200

    push something

--- assets/js/forumbuild.js
+++ assets/js/forumbuild.js
@@ -1,23 +1,3 @@
-var popup = document.getElementById('newpostpopup');
-
-var newpostbutton = document.getElementById("newpostbutton");
-
-var closebutton = document.getElementsByClassName("closebutton")[0];
-
-newpostbutton.onclick = function() {
-    popup.style.display = "block";
-}
-
-closebutton.onclick = function() {
-    popup.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target == popup) {
-        popup.style.display = "none";
-    }
-}
-
 function getLocal () {
 	var notes = localStorage.getItem("noteList")
 
